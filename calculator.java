f8784db3ef1400121a22eb725ac4eb1aff5cfc79
dkiilm823 <deepak.kumar.cs28@iilm.edu>
2025-08-07 11:44:42 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -7,20 +7,23 @@ public class Main {
         int n1 = sc.nextInt();
         System.out.print("Enter n2: ");
         int n2 = sc.nextInt();
-        System.out.println("Enter 1 for addition");
-        System.out.println("Enter 2 for Subtraction");
-        System.out.println("Enter 3 for Division");
-        System.out.println("Enter 4 for Multiplication");
-        System.out.print("Enter the operator number: ");
-        int op = sc.nextInt();
-        if(op == 1){
-            System.out.println(n1+ "+" +n2 +"="  + (n1 + n2));
-        }else if(op == 2){
-            System.out.println(n1+ "-" +n2+ "=" + (n1 - n2));
-        }else if(op == 3){
-            System.out.println(n1+ "/" +n2 +"=" + (n1 / n2));
-        }else{
-            System.out.println(n1+ "*" +n2 +"=" + (n1 * n2));
+        System.out.print("Enter operator +,-,/,* : ");
+        char operator = sc.next().charAt(0);
+        int result = 0;
+        switch (operator){
+            case '+':
+                result = n1+n2;
+                break;
+            case '-':
+                result = n1-n2;
+                break;
+            case '/':
+                result = n1/n2;
+                break;
+            case '*':
+                result = n1*n2;
+                break;
         }
+        System.out.println("Result "+ result);
     }
 }
