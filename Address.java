cd9c97cf9ebd7cc26bbb592b904263546357ec31
dkiilm823 <deepak.kumar.cs28@iilm.edu>
2025-08-07 12:23:19 +0530

    Create Address.java

---
+++ Address.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        // in single line
+        System.out.print("ABC,New-Vihar,India\n");
+        // in multiple line
+        System.out.println("ABC");
+        System.out.println("New-Vihar");
+        System.out.println("India");
+        
+    }
+}
