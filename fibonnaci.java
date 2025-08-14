fd56e31068559845d326e1b1bddb8de3331897cc
dkiilm823 <deepak.kumar.cs28@iilm.edu>
2025-08-14 11:01:45 +0530

    Create fibonnaci.java

---
+++ fibonnaci.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of n:  ");
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+         for (int i = 0; i < n; i++) {
+             System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+            
+        }
+    }
+}
