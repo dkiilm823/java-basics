38d582e6b6c10602a352647bb8db093c38b2c3bd
dkiilm823 <deepak.kumar.cs28@iilm.edu>
2025-08-18 11:31:05 +0530

    Update swap-temp-var.java

--- swap-temp-var.java
+++ swap-temp-var.java
@@ -1,10 +1,10 @@
-public class SwapWithTemp {
+public class SwapWithVar {
     public static void main(String[] args) {
         int a = 5, b = 10;
         System.out.println("Before swap: a = " + a + ", b = " + b);
         int temp = a; 
         a = b;        
         b = temp;  
-        System.out.println("After swap (with 3rd variable): a = " + a + ", b = " + b);
+        System.out.println("After swap : a = " + a + ", b = " + b);
     }
 }
