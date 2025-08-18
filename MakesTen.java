742deecf91ef894d7729b260d2e6286f75230132
dkiilm823 <deepak.kumar.cs28@iilm.edu>
2025-08-18 11:35:14 +0530

    Create MakesTen.java

---
+++ MakesTen.java
@@ -0,0 +1,9 @@
+public static boolean Ten(int a, int b) {
+    if (a+b == 10) {
+        return true;  
+    } else if (a == 10 || b == 10) {
+        return true;   
+    } else {
+        return false; 
+    }
+}
